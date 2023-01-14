9de2582659e2ac978538f591dfbb8d0213668474
erhanurgun <erhan.rgn04@gmail.com>
2023-01-14 19:33:29 +0300

    db.json eklendi ve ufak duzenleme yapildi

--- Responses.py
+++ Responses.py
@@ -1,24 +1,11 @@
-from datetime import datetime
+import json
 
 def sample_response(input_text):
     user_message = str(input_text).lower()
+    with open('db.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        for p in data:
+            if user_message in p['keys']:
+                return p['value']
 
-    if user_message in ("merhaba", "hey"):
-        return "Merhaba dostum hoşgeldin!"
-
-    if user_message in ("selam", "sa", "selamun aleykum", "selamün aleyküm", "selamünaleyküm"):
-        return "Ve aleyna aleyküm selam verahmetullahi veberekatuhu"
-
-    if user_message in ("nasılsın", "nasılsın?"):
-        return "İyiyim, teşekkür ederim. Sen nasılsın?"
-
-    if user_message in ("kimsin sen", "kimsin", "sen kimsin" "sen nesin"):
-        return "Ben BT yardım botuyum. Size nasıl yardımcı olabilirim?"
-
-    if user_message in ("zaman", "saat kaç", "saat"):
-        now = datetime.now()
-        date_time = now.strftime("Tarih: %d/%m/%y - Saat: %H:%M:%S")
-        return str(date_time)
-
-    return "Seni anlamadım ne demek istedin?"
-
+    return "Ne demek istediğini anlayamadım!"
